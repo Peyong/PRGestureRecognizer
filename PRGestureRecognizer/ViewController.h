f0757bfdc3620b16f5705698e7a18f9d93ccc2b8
peirong <peirong@peirongdeimac.local>
2015-12-24 19:16:50 +0800

    Initial Commit

---
+++ PRGestureRecognizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PRGestureRecognizer
+//
+//  Created by peirong on 15/12/24.
+//  Copyright © 2015年 peirong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PRGestureRecognizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PRGestureRecognizer
+//
+//  Created by peirong on 15/12/24.
+//  Copyright © 2015年 peirong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
